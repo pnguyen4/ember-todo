e2a0e22866688d99d52454cba47bcc2a61195865
Phillip Nguyen <pnguyen4711@gmail.com>
2023-02-15 13:07:10 -0500

    fix tests?

--- tests/integration/components/todo-card-test.js
+++ tests/integration/components/todo-card-test.js
@@ -10,17 +10,24 @@ module('Integration | Component | todo-card', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<TodoCard />`);
+    this.set('foo', function (...args) {
+      console.log('run action with args:', args);
+    });
+    await render(
+      hbs`<TodoCard @id="1" @title="test" @description="test" @status="In Progress" @delete={{this.foo}} @update={{this.foo}} />`
+    );
 
-    assert.dom(this.element).hasText('');
+    assert
+      .dom(this.element)
+      .hasText('test (In Progress) id: 1 test Update Status');
 
     // Template block usage:
-    await render(hbs`
-      <TodoCard>
-        template block text
-      </TodoCard>
-    `);
+    await render(
+      hbs`<TodoCard @id="1" @title="test" @description="test" @status="In Progress" @delete={{this.foo}} @update={{this.foo}}>template block text</TodoCard>`
+    );
 
-    assert.dom(this.element).hasText('template block text');
+    assert
+      .dom(this.element)
+      .hasText('test (In Progress) id: 1 test Update Status');
   });
 });
